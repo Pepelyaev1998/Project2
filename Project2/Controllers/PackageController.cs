6ad0fad906e8113e8c6150d08f5eb06049e4d04f
Ilya Pepelyaev <i.pepelyaev@itransition.com>
2024-12-16 13:38:07 +0300

    add report

--- Project2/Controllers/PackageController.cs
+++ Project2/Controllers/PackageController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using Project2.Models;
 using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +15,12 @@ namespace Project2.Controllers
     public class PackageController : Controller
     {
         private DBContext db;
+        private readonly IHostingEnvironment hostingEnvironment;
 
-        public PackageController(DBContext context)
+        public PackageController(DBContext context, IHostingEnvironment hostingEnvironment)
         {
             db = context;
+            this.hostingEnvironment = hostingEnvironment;
         }
 
         [HttpGet]
@@ -116,6 +122,51 @@ namespace Project2.Controllers
                     ModelState.AddModelError("", "Icorrect data");
             }
             return View(packageModel);
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public async Task<IActionResult> ExportToFile()
+        {
+            string webRootFolder = hostingEnvironment.WebRootPath;
+            string fileName = @"Package.xlsx";
+            var memory = new MemoryStream();
+            var workbook = new XSSFWorkbook();
+            var excelSheet = workbook.CreateSheet("Packages");
+            excelSheet.DefaultColumnWidth = 30;
+            var packages = await db.Packages.ToListAsync();
+
+            using (var fs = new FileStream(Path.Combine(webRootFolder, fileName), FileMode.Create, FileAccess.Write))
+            {
+
+                IRow row = excelSheet.CreateRow(0);
+                row.CreateCell(0).SetCellValue("Track Number");
+                row.CreateCell(1).SetCellValue("Starting Point");
+                row.CreateCell(2).SetCellValue("End Point");
+                row.CreateCell(3).SetCellValue("Last Location");
+                row.CreateCell(4).SetCellValue("Current State");
+                row.CreateCell(5).SetCellValue("Last date of update");
+
+                for (int r = 1; r <= packages.Count; r++)
+                {
+                    row = excelSheet.CreateRow(r);
+                    var item = packages[r - 1];
+                    row.CreateCell(0).SetCellValue(item.TrackNumber);
+                    row.CreateCell(1).SetCellValue(item.StartingPoint);
+                    row.CreateCell(2).SetCellValue(item.EndPoint);
+                    //row.GetCell(2).CellStyle.Alignment = HorizontalAlignment.Left;
+                    row.CreateCell(3).SetCellValue(item.LastLocation);
+                    row.CreateCell(4).SetCellValue(item.CurrentState);
+                    row.CreateCell(5).SetCellValue(item.LastDateOfUpdate.ToShortDateString() + " " + item.LastDateOfUpdate.ToShortTimeString());
+                }
+                workbook.Write(fs);
+            }
+            using (var stream = new FileStream(Path.Combine(webRootFolder, fileName), FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
         }
     }
 }
